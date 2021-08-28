0d9f7d11a9ba15d1073380a5b0af8ff96354b44f
Sarah Fuoss <sfuoss@gmail.com>
2021-08-28 18:09:34 -0500

    added separate div for total costs to allow styling

--- assignment/client.js
+++ assignment/client.js
@@ -2,26 +2,37 @@ $(document).ready(readyNow)
 
 function readyNow(){
     console.log('Document Ready');
-    $('#submit-button').on('click', addEmployee);
+    $('#submit-button').on('click', createEmployee);   
 }
 
-function addEmployee(){
+let employeeArray = [];
+
+
+
+function createEmployee(){
     console.log('Button pushed');
     let firstName = $('#first-name').val();
     let lastName = $('#last-name').val();
     let employeeID = $('#employeeID').val();
     let employeeTitle = $('#employee-title').val();
     let employeeSalary = $('#annual-salary').val();
+    employeeArray.push({firstName,lastName,employeeID,employeeTitle,employeeSalary}); //adds employee to end of array
+    addEmployee();
+}
+
+function addEmployee(){
+    let employee = employeeArray[employeeArray.length-1] //references the last employee added
     $('#employee-table').append(`<tr>
-    <td>${firstName}</td>
-    <td>${lastName}</td>
-    <td>${employeeID}</td>
-    <td>${employeeTitle}</td>
-    <td>${employeeSalary}</td>
-    <td><button id="delete-employee">Delete</button></td>`)
-    $('#monthly-costs').empty()
-    totalSalary+= parseInt(employeeSalary); //converts employee salary from string to number
-    $('#monthly-costs').append(`Total Monthly Costs ${totalSalary}`);
+    <td>${employee.firstName}</td>
+    <td>${employee.lastName}</td>
+    <td>${employee.employeeID}</td>
+    <td>${employee.employeeTitle}</td>
+    <td>${employee.employeeSalary}</td>
+    <td><button class="delete-employee">Delete</button></td>`)
+    $('#monthly-cost-value').empty()
+    totalSalary+= parseInt(employee.employeeSalary); //converts employee salary from string to number
+    $('#monthly-cost-value').append(`${totalSalary}`);
+    $('.delete-employee').on('click', deleteEmployee)
 }
 totalSalary = 0;
 
@@ -32,3 +43,7 @@ totalSalary = 0;
     //totalSalary+= $(this).parent(employeeSalary);
     //$('#monthly-costs').append(`$${totalSalary}`);
 //}
+
+function deleteEmployee(){
+    console.log('hey there');
+}
